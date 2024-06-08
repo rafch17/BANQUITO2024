15c243e0bac7798b4a27b1f8556e8c88b5245546
Hendrix <hrcoral@espe.edu.ec>
2024-06-07 23:48:10 -0500

    Fix format

--- bank/src/main/java/com/banquito/core/bank/controller/RoleController.java
+++ bank/src/main/java/com/banquito/core/bank/controller/RoleController.java
@@ -1,5 +1,39 @@
 package com.banquito.core.bank.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banquito.core.bank.model.Role;
+import com.banquito.core.bank.service.CommonBankService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/roles")
 public class RoleController {
+
+    private final CommonBankService service;
+
+    public RoleController(CommonBankService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Role>> getAll() {
+        return ResponseEntity.ok(this.service.obtainAllRoles());
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<Role> getByCode(@PathVariable String code) {
+        try {
+            return ResponseEntity.ok(this.service.obtainRole(code));
+        } catch (RuntimeException rte) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
